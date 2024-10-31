446706870cba774231caf1fe1a8903c9133c5f5c
Sharif Alalem <sharif.alalem@savings-united.com>
2024-10-31 04:12:44 +0100

    add sounds data array

---
+++ src/components/PadBoardComponent/SoundsLib.ts
@@ -0,0 +1,21 @@
+import clap from '../../assets/audios/clap-808.wav';
+import cowbell from '../../assets/audios/cowbell-808.wav';
+import crash from '../../assets/audios/crash-acoustic.wav';
+import hihit from '../../assets/audios/hihat-808.wav';
+import kick from '../../assets/audios/kick-808.wav';
+import openhat from '../../assets/audios/openhat-808.wav';
+import perc from '../../assets/audios/perc-808.wav';
+import shaker from '../../assets/audios/shaker-analog.wav';
+import snare from '../../assets/audios/snare-808.wav';
+
+export const SoundsArr = {
+    _81:{src:clap,text:'Clap'},
+    _87:{src:cowbell,text:'Cowbell'},
+    _69:{src:crash,text:'Crash'},
+    _65:{src:hihit,text:'Hihat'},
+    _83:{src:kick,text:'Kick'},
+    _68:{src:openhat,text:'OpenHat'},
+    _90:{src:perc,text:'Perc'},
+    _88:{src:shaker,text:'Shaker'},
+    _67:{src:snare,text:'Snare'}
+};
